89b17907620100df580554c17d4efe3c80f0fc71
Khadim Fall <khadimfall0602@gmail.com>
2023-10-06 09:22:30 +0200

    ??

--- src/components/custom-select.tsx
+++ src/components/custom-select.tsx
@@ -16,7 +16,7 @@ export default function CustomSelect({ title, items, value, onChange }: CustomSe
     const onChangeHandler = onChange ? (e: any) => onChange(e.target.value) : undefined;
 
     return <div className="p-2 bg-white rounded w-full relative">
-        <span className="top-0 text-black/40 text-sm bg-white">{title}</span>
+        <span className="top-0 text-black/40 w-full text-sm bg-white">{title}</span>
         <select value={value} onChange={onChangeHandler} className="w-full text-xl outline-none">
             {(items || []).map((item, i) => <option key={i} value={item.value}>{item.label}</option>)}
         </select>
